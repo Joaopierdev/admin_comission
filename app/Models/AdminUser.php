3a9b88838539d15ae5cb4caa21312bc48a131c3f
João Gabriel <127800063+Joaopierdev@users.noreply.github.com>
2025-12-17 17:46:59 -0300

    Criando Service de UserAdmin para criação de um Seller

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class AdminUser extends Model
 {
-    //
+    // use HasFactory;
+    protected $table = 'admin_users';
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_admin',
+    ];
+
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
